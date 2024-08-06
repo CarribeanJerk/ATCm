614c78f395c2372f93ea766a49cb68101417f0bb
joshua sack <joshsack99@gmail.com>
2024-08-06 02:17:26 -0400

    clear

--- root-directory/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const page = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="font-bold text-center">Discover</h1>
-    </section>
-  )
-}
-
-export default page
