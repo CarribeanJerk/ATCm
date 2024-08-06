43f1dcc660e822706264c2bc56d57dd3af09d7a2
joshua sack <joshsack99@gmail.com>
2024-08-06 02:13:24 -0400

    init

---
+++ root-directory/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="font-bold text-center">Discover</h1>
+    </section>
+  )
+}
+
+export default page
